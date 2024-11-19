7adbef7c1998b1abf445449cce977581ddaf7804
RaphStille <raphael.stille@student.hepl.be>
2024-11-19 20:04:12 +0100

    🔧 Simplification

--- src/js/main.js
+++ src/js/main.js
@@ -1,41 +1,17 @@
 (function () {
-    const navChange = {
-        navElement : document.getElementById('navigation'),
-        button : document.querySelector('.nav-button'),
-        init(){
-            this.button.addEventListener('click', (evt) => {
-                this.buttonNav(evt);
-            })
-            window.addEventListener('resize', ()=>{
-                this.navigationChange()
+    const burgerMenu = {
+        eMain : document.querySelector('.main'),
+        eNavButton : document.querySelector('.nav-button'),
+
+        init () {
+            document.documentElement.classList.add('js-enabled');
+            this.eNavButton.addEventListener('click',(event)=> {
+                this.classChange();
             });
         },
-        navigationChange() {
-            if (window.innerWidth > 544) {
-                if (this.navElement) {
-                    this.navElement.style.display = 'block';
-                    this.button.style.display = 'none'
-                }
-            } else {
-                if (this.navElement) {
-                    this.navElement.style.display = 'none'
-                    this.button.style.display ='block'
-                }
-            }
-        },
-        buttonNav(evt){
-            document.querySelector('.main').classList.toggle('is-opened');
-            if(this.navElement.style.display === 'block'){
-                this.navElement.style.display = 'none'
-            } else{
-                this.navElement.style.display = 'block'
-            }
-            if (this.navElement) {
-                this.navElement.focus();
-            }
-            evt.preventDefault();
+        classChange(){
+            this.eMain.classList.toggle('is-opened');
         }
     }
-
-    navChange.init();
+    burgerMenu.init();
 })();
